8b2dbc9ba56978101b921d118559635f8b4a2f54
seokjae hong <hsj2334@gmail.com>
2018-03-18 20:15:33 +0900

    템플릿을 이용한 메인 페이지 Html 생성

--- functional_test.py
+++ functional_test.py
@@ -1,49 +1,46 @@
+import time
 from selenium import webdriver
 import unittest
-
-
-#
-# browser = webdriver.Chrome()
-# browser.get('http://localhost:8000')
-# assert 'To-Do' in browser.title
-# browser.quit()
 from selenium.webdriver.common.keys import Keys
 
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
+        # self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-do', header_text)
+        self.assertIn('To-Do', header_text)
 
-        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
         )
-        print(inputbox)
+        time.sleep(1)
+        # inputbox.send_keys('1:공작깃털 a')
+        # time.sleep(1)
+        # inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        inputbox.send_keys('공작 깃털 사기')
-
-        inputbox.send_keys(Keys.ENTER)
-
-        table =self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text=='1:공작깃털사기' for row in rows),
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다."
         )
 
         self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
