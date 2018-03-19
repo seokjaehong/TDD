c24b5344b11eac3e7691ee758379f7afee5bcae5
seokjae hong <hsj2334@gmail.com>
2018-03-19 22:53:33 +0900

    firefox로 브라우저변경하고, 4장끝

--- functional_test.py
+++ functional_test.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
         # self.browser.implicitly_wait(10)
 
     def tearDown(self):
@@ -20,23 +20,23 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+
         inputbox = self.browser.find_element_by_id('id_new_item')
 
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             '작업 아이템 입력'
         )
+        inputbox.send_keys('1:공작깃털 사기')
         time.sleep(1)
-        # inputbox.send_keys('1:공작깃털 a')
-        # time.sleep(1)
-        # inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
+        print(table.text)
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: 공작깃털 사기' for row in rows),
-            "신규 작업이 테이블에 표시되지 않는다."
+            "신규 작업이 테이블에 표시되지 않는다. 해당 텍스트 :\n%s" % (table.text,)
         )
 
         self.fail('Finish the test!')
--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
@@ -9,16 +11,41 @@ from lists.views import home_page
 # Create your tests here.
 
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+
     def test_root_url_resloves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        # response = home_page(request)
+        # expected_html = render_to_string('home.html')
+
+
+        # request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
+
+        # self.assertEqual(response.content.decode(), expected_html)
 
     # def test_home_page_returns_correct_html(self):
     #     request = HttpRequest()
     #     response = home_page(request)
     #     expected_html = render_to_string('home.html')
     #     self.assertEqual(response.content.decode(), expected_html)
-        # self.assertTrue(response.content.strip().startswith(b'<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.strip().endswith(b'</html>'))
-        # #
+    # self.assertTrue(response.content.strip().startswith(b'<html>'))
+    # self.assertIn(b'<title>To-Do lists</title>', response.content)
+    # self.assertTrue(response.content.strip().endswith(b'</html>'))
+    # #
