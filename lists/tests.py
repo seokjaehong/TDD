67577d93eb7fbb27f3c87490a5f6da4dce8ce453
seokjae hong <hsj2334@gmail.com>
2018-03-21 18:41:51 +0900

    아이템용 모델과 마이그레이션 연계

--- lists/tests.py
+++ lists/tests.py
@@ -6,60 +6,77 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 
 from lists.views import home_page
+from lists.models import Item
 
 
 # Create your tests here.
 
-class HomePageTest(TestCase):
-    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+# class HomePageTest(TestCase):
+#     pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+#
+#     def test_root_url_resloves_to_home_page_view(self):
+#         found = resolve('/')
+#         self.assertEqual(found.func, home_page)
+#     #
+#     def test_home_page_can_save_a_POST_request(self):
+#         request = HttpRequest()
+#         request.method = 'POST'
+#         request.POST['item_text'] = '신규 작업 아이템'
+#         response = home_page(request)
+#
+#         self.assertIn('신규 작업 아이템', response.content.decode())
+#         expected_html = render_to_string(
+#             'home.html',
+#             {'new_item_text': '신규 작업 아이템'}
+#         )
+#         self.assertEqual(
+#             re.sub(self.pattern_input_csrf, '', response.content.decode()),
+#             re.sub(self.pattern_input_csrf, '', expected_html)
+#         )
+#
+#     # def test_home_page_returns_correct_html(self):
+#     #     request = HttpRequest()
+#     #     response = home_page(request)
+#     #     expected_html = render_to_string('home_html',{'new_item_text': '신규 작업 아이템'})
+#     #     # print(expected_html)
+#     #
+#     #     self.assertEqual(response.content.decode(), expected_html)
+#
+#     #
+#     def test_home_page_can_save_a_POST_request(self):
+#         request = HttpRequest()
+#         request.method = 'POST'
+#         request.POST['item_text'] = '신규 작업 아이템'
+#         response = home_page(request)
+#         # print(response.content)
+#
+#         self.assertIn('신규 작업 아이템', response.content.decode())
+#         expected_html = render_to_string(
+#             'home.html',
+#             {'new_item_text': '신규 작업 아이템'}
+#         )
+#         print(expected_html)
+#         # print(expected_html)
+#         print(response.content.decode())
+#         self.assertEqual(response.content.decode(), expected_html)
+#         # self.assertEqual(response.content.decode(), expected_html)
 
-    def test_root_url_resloves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        # response = home_page(request)
-        # expected_html = render_to_string('home.html')
+class ItemModelTest(TestCase):
 
-        # request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-        response = home_page(request)
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(
-            re.sub(self.pattern_input_csrf, '', response.content.decode()),
-            re.sub(self.pattern_input_csrf, '', expected_html)
-        )
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
 
-    def test_home_page_returns_correct_html(self):
-        pass
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': '신규 작업 아이템'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
-
-    # def test_home_page_returns_correct_html(self):
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     expected_html = render_to_string('home.html')
-    #     self.assertEqual(response.content.decode(), expected_html)
-    # self.assertTrue(response.content.strip().startswith(b'<html>'))
-    # self.assertIn(b'<title>To-Do lists</title>', response.content)
-    # self.assertTrue(response.content.strip().endswith(b'</html>'))
-    # #
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,14 @@ from django.shortcuts import render
 # home_page = None
 
 def home_page(request):
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    # return HttpResponse()
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html', {
+        # 'new_item_text': request.POST['item_text'],
         'new_item_text': request.POST.get('item_text',''),
     })
+
+    # return render(request, 'home.html', {
+    # 'new_item_text': request.POST.get('item_text',''),
+    # 'new_item_text': request.POST['item_text'],
+    # })
