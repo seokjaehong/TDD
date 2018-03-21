f98d38a1a9d4193d0ccc0f1bd0c00935a05d7fe5
seokjae hong <hsj2334@gmail.com>
2018-03-21 16:50:04 +0900

    functional_test.py 변경내용

--- functional_test.py
+++ functional_test.py
@@ -14,6 +14,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -21,43 +30,29 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # inputbox = self.browser.find_element_by_id('id_new_item')
-
-        # self.assertEqual(
-        #     inputbox.get_attribute('placeholder'),
-        #     '작업 아이템 입력'
-        # )
-        # inputbox.send_keys('1. 공작깃털 사기')
-        # # time.sleep(1)
-        # inputbox.send_keys(Keys.ENTER)
-        # # import time
-        # # time.sleep(10)
-        #
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # # self.assertTrue(
-        # #     any(row.text == '1. 공작깃털 사기' for row in rows),
-        # #     "신규 작업이 테이블에 표시되지 않는다. 해당 텍스트 :\n%s" % (
-        # #         table.text,
-        # #     )
-        # # )
-        # self.assertIn('1:공작깃털 사기', [row.text for row in rows])
-        #
-        # self.fail('Finish the test!')
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('공작깃털을 이용해서 그물만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        time.sleep(3)
+        time.sleep(2)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기',
-            [row.text for row in rows]
-        )
-        self.fail('Finish the test!')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물만들기')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        # self.assertIn(
+        #     '2: 공작깃털을 이용해서 그물 만들기',
+        #     [row.text for row in rows]
+        # )
+        # self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
