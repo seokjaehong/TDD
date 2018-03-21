faf32396264493fa5ea54d9e5925d42a535baf89
seokjae hong <hsj2334@gmail.com>
2018-03-21 20:21:48 +0900

    functional_tests 앱 생성 및 LiveServerTestCase 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,9 +3,10 @@ import time
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -24,14 +25,15 @@ class NewVisitorTest(unittest.TestCase):
         # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('공작깃털을 이용해서 그물만들기')
+        inputbox.send_keys('공작깃털 사기')
         inputbox.send_keys(Keys.ENTER)
 
         time.sleep(2)
@@ -42,8 +44,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('공작깃털을 이용해서 그물만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
+        time.sleep(2)
+
         self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
 
         # table = self.browser.find_element_by_id('id_list_table')
         # rows = table.find_elements_by_tag_name('tr')
@@ -55,5 +59,5 @@ class NewVisitorTest(unittest.TestCase):
         # self.fail('Finish the test!')
 
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+# if __name__ == '__main__':
+#     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ import re
 
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+# from django.template.loader import render_to_string
 from django.template.loader import render_to_string
 from django.test import TestCase
 
@@ -11,55 +12,59 @@ from lists.models import Item
 
 # Create your tests here.
 
-# class HomePageTest(TestCase):
-#     pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
-#
-#     def test_root_url_resloves_to_home_page_view(self):
-#         found = resolve('/')
-#         self.assertEqual(found.func, home_page)
-#     #
-#     def test_home_page_can_save_a_POST_request(self):
-#         request = HttpRequest()
-#         request.method = 'POST'
-#         request.POST['item_text'] = '신규 작업 아이템'
-#         response = home_page(request)
-#
-#         self.assertIn('신규 작업 아이템', response.content.decode())
-#         expected_html = render_to_string(
-#             'home.html',
-#             {'new_item_text': '신규 작업 아이템'}
-#         )
-#         self.assertEqual(
-#             re.sub(self.pattern_input_csrf, '', response.content.decode()),
-#             re.sub(self.pattern_input_csrf, '', expected_html)
-#         )
-#
-#     # def test_home_page_returns_correct_html(self):
-#     #     request = HttpRequest()
-#     #     response = home_page(request)
-#     #     expected_html = render_to_string('home_html',{'new_item_text': '신규 작업 아이템'})
-#     #     # print(expected_html)
-#     #
-#     #     self.assertEqual(response.content.decode(), expected_html)
-#
-#     #
-#     def test_home_page_can_save_a_POST_request(self):
-#         request = HttpRequest()
-#         request.method = 'POST'
-#         request.POST['item_text'] = '신규 작업 아이템'
-#         response = home_page(request)
-#         # print(response.content)
-#
-#         self.assertIn('신규 작업 아이템', response.content.decode())
-#         expected_html = render_to_string(
-#             'home.html',
-#             {'new_item_text': '신규 작업 아이템'}
-#         )
-#         print(expected_html)
-#         # print(expected_html)
-#         print(response.content.decode())
-#         self.assertEqual(response.content.decode(), expected_html)
-#         # self.assertEqual(response.content.decode(), expected_html)
+class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+
+    def test_root_url_resloves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
+        # expected_html = render_to_string('home.html')
+
+        self.assertEquals(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+        # print(response)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+        # print(response)
+        # print(response.content.decode())
+
+        self.assertIn('itemey 1', str(response.content.decode()))
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
